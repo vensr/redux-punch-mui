e5f1f8eb0396432cd47624d2e9b35974288fcc90
venkatesh <>
2022-09-07 00:06:46 +0530

    redux devtools update and audit fixes

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from '@redux-devtools/extension';
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { loading, message } from "../reducers/common/reducers";
 import { STORE_NAME } from "../EnvVars";
